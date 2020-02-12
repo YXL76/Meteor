5b98f0cc7e9f9739950564825d663d3e730f986a
YXL <chenxin.lan.76@gmail.com>
2020-02-12 23:17:11 +0800

    add dark mode and search, style update

--- static/js/onscroll.js
+++ static/js/onscroll.js
@@ -5,50 +5,44 @@ function Dropdown(event) {
     mask.display = checkbox ? "block" : "none";
 };
 
-function scroollAction(winTop) {
-    if (!checkbox) {
-        if (winTop >= wintopPre) {
+let heightDiff;
+let wintopPre = 100000000;
+let scrollLine, header, mask;
+
+function moveHeader(winTop) {
+    if (!checkbox && Math.abs(winTop - wintopPre) > 4) {
+        if (winTop > wintopPre) {
             header.transform = "translateY(-60px)";
         } else {
             header.transform = "translateY(0)";
         }
     }
     wintopPre = winTop;
-};
+}
 
-let heightDiff;
+function moveLine(winTop) {
+    const scrolled = winTop * heightDiff;
+    scrollLine.width = scrolled + "%";
+}
 
 function resize() {
     heightDiff = 100 / (document.body.scrollHeight - window.innerHeight);
-    scroollAction(document.documentElement.scrollTop);
-};
-
-let wintopPre = 100000000;
-let scrollLine, header, mask;
+    moveLine(document.body.scrollTop || document.documentElement.scrollTop);
+}
 
 window.onload = () => {
     document.getElementById("dropdown-menu").checked = false;
     scrollLine = document.getElementsByClassName("scroll-line")[0].style;
     header = document.getElementsByClassName("header")[0].style;
-    mask = document.getElementsByClassName("mask")[0].style
+    mask = document.getElementsByClassName("mask")[0].style;
     resize();
 };
 
 document.body.onresize = resize;
-
 window.onresize = resize;
-
-let onscrollTimer = true;
 
 window.onscroll = () => {
     const winTop = document.body.scrollTop || document.documentElement.scrollTop;
-    const scrolled = winTop * heightDiff;
-    scrollLine.width = scrolled + "%";
-    if (onscrollTimer) {
-        setTimeout(() => {
-            onscrollTimer = true;
-            scroollAction(winTop);
-        }, 200); 
-        onscrollTimer = false;
-    }
+    moveLine(winTop);
+    moveHeader(winTop);
 };
